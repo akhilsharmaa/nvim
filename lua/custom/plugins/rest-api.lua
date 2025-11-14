a4662cbfe73aa96ac3325b89fb1b478f0b6ebb17
Akhilesh <akhilsharma3332@gmail.com>
2025-11-14 09:50:59 +0530

    api: rest added.

---
+++ lua/custom/plugins/rest-api.lua
@@ -0,0 +1,14 @@
+return {
+  'mistweaverco/kulala.nvim',
+  keys = {
+    { '<leader>Rs', '<cmd>lua require("kulala").run()<cr>', desc = 'Send request' },
+    { '<leader>Ra', '<cmd>lua require("kulala").run_all()<cr>', desc = 'Send all requests' },
+    { '<leader>Rb', '<cmd>lua require("kulala").scratchpad()<cr>', desc = 'Open scratchpad' },
+  },
+  ft = { 'http', 'rest' },
+  opts = {
+    global_keymaps = false,
+    global_keymaps_prefix = '<leader>R',
+    kulala_keymaps_prefix = '',
+  },
+}
