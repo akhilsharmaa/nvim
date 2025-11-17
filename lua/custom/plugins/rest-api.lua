42a5fc56f25f29ac9b5312ce8d051aed3b14897a
Akhilesh <akhilsharma3332@gmail.com>
2025-11-17 19:22:37 +0530

    horigontal -> floating

--- lua/custom/plugins/rest-api.lua
+++ lua/custom/plugins/rest-api.lua
@@ -1,14 +1,15 @@
 return {
-  'mistweaverco/kulala.nvim',
-  keys = {
-    { '<leader>Rs', '<cmd>lua require("kulala").run()<cr>', desc = 'Send request' },
-    { '<leader>Ra', '<cmd>lua require("kulala").run_all()<cr>', desc = 'Send all requests' },
-    { '<leader>Rb', '<cmd>lua require("kulala").scratchpad()<cr>', desc = 'Open scratchpad' },
-  },
-  ft = { 'http', 'rest' },
-  opts = {
-    global_keymaps = false,
-    global_keymaps_prefix = '<leader>R',
-    kulala_keymaps_prefix = '',
-  },
+	'mistweaverco/kulala.nvim',
+	keys = {
+		{ '<leader>Rs', '<cmd>lua require("kulala").run()<cr>',        desc = 'Send request' },
+		{ '<leader>Ra', '<cmd>lua require("kulala").run_all()<cr>',    desc = 'Send all requests' },
+		{ '<leader>Rb', '<cmd>lua require("kulala").scratchpad()<cr>', desc = 'Open scratchpad' },
+	},
+	ft = { 'http', 'rest' },
+	opts = {
+		global_keymaps = false,
+		global_keymaps_prefix = '<leader>R',
+		kulala_keymaps_prefix = '',
+		display_mode = 'float', -- show response in a floating window
+	},
 }
