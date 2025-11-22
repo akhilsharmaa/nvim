41f26e86e85429774352ff837f9f32dc05cfb756
Akhilesh <akhilsharma3332@gmail.com>
2025-11-22 11:16:27 +0530

    minimap + startup added

---
+++ lua/custom/plugins/minimap.lua
@@ -0,0 +1,49 @@
+return {
+	"Isrothy/neominimap.nvim",
+	version = "v3.x.x",
+	lazy = false,
+	keys = {
+		{ "<leader>nm",  "<cmd>Neominimap Toggle<cr>",      desc = "Toggle global minimap" },
+		{ "<leader>no",  "<cmd>Neominimap Enable<cr>",      desc = "Enable global minimap" },
+		{ "<leader>nc",  "<cmd>Neominimap Disable<cr>",     desc = "Disable global minimap" },
+		{ "<leader>nr",  "<cmd>Neominimap Refresh<cr>",     desc = "Refresh global minimap" },
+		{ "<leader>nwt", "<cmd>Neominimap WinToggle<cr>",   desc = "Toggle minimap for current window" },
+		{ "<leader>nwr", "<cmd>Neominimap WinRefresh<cr>",  desc = "Refresh minimap for current window" },
+		{ "<leader>nwo", "<cmd>Neominimap WinEnable<cr>",   desc = "Enable minimap for current window" },
+		{ "<leader>nwc", "<cmd>Neominimap WinDisable<cr>",  desc = "Disable minimap for current window" },
+		{ "<leader>ntt", "<cmd>Neominimap TabToggle<cr>",   desc = "Toggle minimap for current tab" },
+		{ "<leader>ntr", "<cmd>Neominimap TabRefresh<cr>",  desc = "Refresh minimap for current tab" },
+		{ "<leader>nto", "<cmd>Neominimap TabEnable<cr>",   desc = "Enable minimap for current tab" },
+		{ "<leader>ntc", "<cmd>Neominimap TabDisable<cr>",  desc = "Disable minimap for current tab" },
+		{ "<leader>nbt", "<cmd>Neominimap BufToggle<cr>",   desc = "Toggle minimap for current buffer" },
+		{ "<leader>nbr", "<cmd>Neominimap BufRefresh<cr>",  desc = "Refresh minimap for current buffer" },
+		{ "<leader>nbo", "<cmd>Neominimap BufEnable<cr>",   desc = "Enable minimap for current buffer" },
+		{ "<leader>nbc", "<cmd>Neominimap BufDisable<cr>",  desc = "Disable minimap for current buffer" },
+		{ "<leader>nf",  "<cmd>Neominimap Focus<cr>",       desc = "Focus on minimap" },
+		{ "<leader>nu",  "<cmd>Neominimap Unfocus<cr>",     desc = "Unfocus minimap" },
+		{ "<leader>ns",  "<cmd>Neominimap ToggleFocus<cr>", desc = "Switch focus on minimap" },
+	},
+	init = function()
+		vim.opt.wrap = false
+		vim.opt.sidescrolloff = 20
+		---@type Neominimap.UserConfig
+		vim.g.neominimap = {
+			auto_enable = true,
+			-- Use braille characters for denser, more connected appearance
+			minimap_char = "⣿",
+			-- Smaller width
+			window = {
+				width = 15,
+			},
+			-- Viewport highlight line
+			viewport = {
+				enabled = true,
+				highlight = "NeominimapViewport",
+			},
+		}
+		-- Background color
+		vim.api.nvim_set_hl(0, "NeominimapNormal", { bg = "#1e1e2e" })
+		-- Viewport line color
+		vim.api.nvim_set_hl(0, "NeominimapViewport", { bg = "#45475a" })
+	end,
+}
