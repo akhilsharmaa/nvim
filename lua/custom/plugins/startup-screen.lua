c0029941da873e37780d043417f2692f340711d2
Akhilesh <akhilsharma3332@gmail.com>
2025-11-22 12:28:48 +0530

    centered

--- lua/custom/plugins/startup-screen.lua
+++ lua/custom/plugins/startup-screen.lua
@@ -1,78 +1,136 @@
+local daytext = function()
+	-- Get full name of current weekday
+	local dayname = os.date("%A")
+
+	-- Big ASCII art for each day
+	local ascii_days = {
+		Monday = {
+			[[]],
+			[[███    ███  ██████  ███    ██ ██████   █████  ██    ██]],
+			[[████  ████ ██    ██ ████   ██ ██   ██ ██   ██  ██  ██ ]],
+			[[██ ████ ██ ██    ██ ██ ██  ██ ██   ██ ███████   ████  ]],
+			[[██  ██  ██ ██    ██ ██  ██ ██ ██   ██ ██   ██    ██   ]],
+			[[██      ██  ██████  ██   ████ ██████  ██   ██    ██   ]],
+		},
+		Tuesday = {
+			[[]],
+			[[████████ ██    ██ ███████ ███████ ██████   █████  ██    ██]],
+			[[   ██    ██    ██ ██      ██      ██   ██ ██   ██  ██  ██ ]],
+			[[   ██    ██    ██ █████   ███████ ██   ██ ███████   ████  ]],
+			[[   ██    ██    ██ ██           ██ ██   ██ ██   ██    ██   ]],
+			[[   ██     ██████  ███████ ███████ ██████  ██   ██    ██   ]],
+		},
+		Wednesday = {
+			[[]],
+			[[ ██     ██ ███████ ██████  ███    ██ ███████ ███████ ███████ ██████   █████  ██    ██]],
+			[[ ██     ██ ██      ██   ██ ████   ██ ██      ██      ██      ██   ██ ██   ██  ██  ██ ]],
+			[[ ██  █  ██ █████   ██   ██ ██ ██  ██ █████   ███████ ███████ ██   ██ ███████   ████  ]],
+			[[ ██ ███ ██ ██      ██   ██ ██  ██ ██ ██           ██      ██ ██   ██ ██   ██    ██   ]],
+			[[  ███ ███  ███████ ██████  ██   ████ ███████ ███████ ███████ ██████  ██   ██    ██   ]],
+		},
+		Thursday = {
+			[[]],
+			[[████████ ██   ██ ██    ██ ██████  ███████ ██████   █████  ██    ██ ]],
+			[[   ██    ██   ██ ██    ██ ██   ██ ██      ██   ██ ██   ██  ██  ██  ]],
+			[[   ██    ███████ ██    ██ ██████  ███████ ██   ██ ███████   ████   ]],
+			[[   ██    ██   ██ ██    ██ ██   ██      ██ ██   ██ ██   ██    ██    ]],
+			[[   ██    ██   ██  ██████  ██   ██ ███████ ██████  ██   ██    ██    ]],
+		},
+		Friday = {
+			[[]],
+			[[███████ ██████  ██ ██████   █████  ██    ██ ]],
+			[[██      ██   ██ ██ ██   ██ ██   ██  ██  ██  ]],
+			[[█████   ██████  ██ ██   ██ ███████   ████   ]],
+			[[██      ██   ██ ██ ██   ██ ██   ██    ██    ]],
+			[[██      ██   ██ ██ ██████  ██   ██    ██    ]],
+		},
+		Saturday = {
+			[[]],
+			[[███████  █████  ████████ ██    ██ ██████  ██████   █████  ██    ██ ]],
+			[[██      ██   ██    ██    ██    ██ ██   ██ ██   ██ ██   ██  ██  ██  ]],
+			[[███████ ███████    ██    ██    ██ ██████  ██   ██ ███████   ████   ]],
+			[[     ██ ██   ██    ██    ██    ██ ██   ██ ██   ██ ██   ██    ██    ]],
+			[[███████ ██   ██    ██     ██████  ██   ██ ██████  ██   ██    ██    ]],
+		},
+		Sunday = {
+			[[]],
+			[[███████ ██    ██ ███    ██ ██████   █████  ██    ██ ]],
+			[[██      ██    ██ ████   ██ ██   ██ ██   ██  ██  ██  ]],
+			[[███████ ██    ██ ██ ██  ██ ██   ██ ███████   ████   ]],
+			[[     ██ ██    ██ ██  ██ ██ ██   ██ ██   ██    ██    ]],
+			[[███████  ██████  ██   ████ ██████  ██   ██    ██    ]],
+		},
+	}
+	return ascii_days[dayname]
+end
+
+-- ~/.config/nvim/lua/plugins/alpha.lua
 return {
 	"goolord/alpha-nvim",
+	event = "VimEnter",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		local startify = require("alpha.themes.startify")
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
 
-		-- Get full name of current weekday
-		local dayname = os.date("%A")
 
-		-- Big ASCII art for each day
-		local ascii_days = {
-			Monday = {
-				[[]],
-				[[███    ███  ██████  ███    ██ ██████   █████  ██    ██]],
-				[[████  ████ ██    ██ ████   ██ ██   ██ ██   ██  ██  ██ ]],
-				[[██ ████ ██ ██    ██ ██ ██  ██ ██   ██ ███████   ████  ]],
-				[[██  ██  ██ ██    ██ ██  ██ ██ ██   ██ ██   ██    ██   ]],
-				[[██      ██  ██████  ██   ████ ██████  ██   ██    ██   ]],
-			},
-			Tuesday = {
-				[[]],
-				[[████████ ██    ██ ███████ ███████ ██████   █████  ██    ██]],
-				[[   ██    ██    ██ ██      ██      ██   ██ ██   ██  ██  ██ ]],
-				[[   ██    ██    ██ █████   ███████ ██   ██ ███████   ████  ]],
-				[[   ██    ██    ██ ██           ██ ██   ██ ██   ██    ██   ]],
-				[[   ██     ██████  ███████ ███████ ██████  ██   ██    ██   ]],
-			},
-			Wednesday = {
-				[[]],
-				[[ ██     ██ ███████ ██████  ███    ██ ███████ ███████ ███████ ██████   █████  ██    ██]],
-				[[ ██     ██ ██      ██   ██ ████   ██ ██      ██      ██      ██   ██ ██   ██  ██  ██ ]],
-				[[ ██  █  ██ █████   ██   ██ ██ ██  ██ █████   ███████ ███████ ██   ██ ███████   ████  ]],
-				[[ ██ ███ ██ ██      ██   ██ ██  ██ ██ ██           ██      ██ ██   ██ ██   ██    ██   ]],
-				[[  ███ ███  ███████ ██████  ██   ████ ███████ ███████ ███████ ██████  ██   ██    ██   ]],
-			},
-			Thursday = {
-				[[]],
-				[[████████ ██   ██ ██    ██ ██████  ███████ ██████   █████  ██    ██ ]],
-				[[   ██    ██   ██ ██    ██ ██   ██ ██      ██   ██ ██   ██  ██  ██  ]],
-				[[   ██    ███████ ██    ██ ██████  ███████ ██   ██ ███████   ████   ]],
-				[[   ██    ██   ██ ██    ██ ██   ██      ██ ██   ██ ██   ██    ██    ]],
-				[[   ██    ██   ██  ██████  ██   ██ ███████ ██████  ██   ██    ██    ]],
-			},
-			Friday = {
-				[[]],
-				[[███████ ██████  ██ ██████   █████  ██    ██ ]],
-				[[██      ██   ██ ██ ██   ██ ██   ██  ██  ██  ]],
-				[[█████   ██████  ██ ██   ██ ███████   ████   ]],
-				[[██      ██   ██ ██ ██   ██ ██   ██    ██    ]],
-				[[██      ██   ██ ██ ██████  ██   ██    ██    ]],
-			},
-			Saturday = {
-				[[]],
-				[[███████  █████  ████████ ██    ██ ██████  ██████   █████  ██    ██ ]],
-				[[██      ██   ██    ██    ██    ██ ██   ██ ██   ██ ██   ██  ██  ██  ]],
-				[[███████ ███████    ██    ██    ██ ██████  ██   ██ ███████   ████   ]],
-				[[     ██ ██   ██    ██    ██    ██ ██   ██ ██   ██ ██   ██    ██    ]],
-				[[███████ ██   ██    ██     ██████  ██   ██ ██████  ██   ██    ██    ]],
-			},
-			Sunday = {
-				[[]],
-				[[███████ ██    ██ ███    ██ ██████   █████  ██    ██ ]],
-				[[██      ██    ██ ████   ██ ██   ██ ██   ██  ██  ██  ]],
-				[[███████ ██    ██ ██ ██  ██ ██   ██ ███████   ████   ]],
-				[[     ██ ██    ██ ██  ██ ██ ██   ██ ██   ██    ██    ]],
-				[[███████  ██████  ██   ████ ██████  ██   ██    ██    ]],
-			},
+		-- Header
+		dashboard.section.header.val = daytext()
+
+		-- Buttons
+		dashboard.section.buttons.val = {
+			dashboard.button("f", "󰈞  Find file", ":Telescope find_files<CR>"),
+			dashboard.button("n", "󰝒  New file", ":ene <BAR> startinsert<CR>"),
+			dashboard.button("r", "󰋚  Recent files", ":Telescope oldfiles<CR>"),
+			dashboard.button("g", "󰊄  Find text", ":Telescope live_grep<CR>"),
+			dashboard.button("c", "  Configuration", ":e $MYVIMRC<CR>"),
+			dashboard.button("s", "󰦛  Restore session", ":SessionRestore<CR>"),
+			dashboard.button("l", "󰒲  Lazy", ":Lazy<CR>"),
+			dashboard.button("q", "󰗼  Quit", ":qa<CR>"),
 		}
 
-		-- Assign big ASCII header for the current weekday
-		startify.section.header.val = ascii_days[dayname] or { "Unknown Day!" }
+		-- Footer with stats
+		local function footer()
+			local stats = require("lazy").stats()
+			local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+			return " Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms"
+		end
 
-		-- Use devicons
-		startify.file_icons.provider = "devicons"
+		dashboard.section.footer.val = footer()
 
-		require("alpha").setup(startify.config)
+		-- Highlight groups
+		dashboard.section.header.opts.hl = "AlphaHeader"
+		dashboard.section.buttons.opts.hl = "AlphaButtons"
+		dashboard.section.footer.opts.hl = "AlphaFooter"
+
+		-- Layout
+		dashboard.opts.layout = {
+			{ type = "padding", val = 2 },
+			dashboard.section.header,
+			{ type = "padding", val = 2 },
+			dashboard.section.buttons,
+			{ type = "padding", val = 1 },
+			dashboard.section.footer,
+		}
+
+		-- Disable folding on alpha buffer
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "alpha",
+			callback = function()
+				vim.opt_local.foldenable = false
+				vim.opt_local.cursorline = false
+			end,
+		})
+
+		alpha.setup(dashboard.opts)
+
+		-- Update footer after lazy has loaded
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "LazyVimStarted",
+			callback = function()
+				dashboard.section.footer.val = footer()
+				pcall(vim.cmd.AlphaRedraw)
+			end,
+		})
 	end,
 }
